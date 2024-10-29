729770830bdaefd2365d2ed998e55c6280b4fd36
Dilankl30 <lucerodk7@gmail.com>
2024-10-29 09:32:13 -0500

    primer commit

---
+++ src/main/java/ec/edu/spoch/mavenproject1/Mavenproject1.java
@@ -0,0 +1,115 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package ec.edu.spoch.mavenproject1;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        /*COMO DECLARAR VARIABLE Y TIPOS DE NUMEROS
+        var nom = "Dila Lucero";
+        var edad = 19;
+            System.out.println("MY name is: " + nom);
+        int n1 = 20;
+        int n2 = 40;
+        int r = n1 + n2;
+            System.out.println("I have " + r + " years old");
+            System.out.println("How are my frineds?");
+TIPOS DE NUMEROS: HEXADECIMALES, OCTALES, BINARIOS
+        var hex = 0xc;
+        var oct = 013;
+        var bin = 0b101;
+            System.out.println("hex" + hex + "oct" + oct + "bin" + bin);*/
+
+//Parcear: convertir el tipo de dato
+        /* 
+        int n1 = 6;
+        String n2 = "6";
+        int r = n1 + (Integer.parseInt(n2));
+        System.out.println("el resultado es:" + r);
+
+//condicional simple
+        int edad = 21;
+        if (edad >= 18) {
+            System.out.println("mayor de eddad");
+        }
+
+        int lad = 4;
+        if (lad == 3) 
+        {
+            System.out.println(" es un triangulo");
+
+        }
+        if (lad==4)
+            {System.out.println("es un cuadrado");}*/
+// BICONDICIONAL 
+        /*  int eda=17;
+        if(eda>=18){System.out.println("eres mayor de edad");}
+        else{System.out.println("eres menor de edad");}
+        
+    }*/
+//Anidada
+        /* int edad = 10;
+        if (edad > 0) {
+            if (edad >= 18) {
+                System.out.println("mayor de edad");
+            } else {
+                System.out.println("eres menor de edad");
+            }
+        } else {
+            System.out.println("el valor es incorrecto");
+     }*/
+        //SWITCH-CASE
+        /*int n1 = 5;
+        int n2 = 0;
+        int menu = 5;
+        switch (menu) {
+            case 1:
+                System.out.println("la suma es:" + (n1 + n2));
+                break;
+            case 2:
+                System.out.println("la resta es:" + (n1 - n2));
+                break;
+            case 3:
+                System.out.println("la multiplicasion es:" + (n1 * n2));
+                break;
+            case 4:
+                if (n2 != 0) {
+                    System.out.println("la division es:" + (n1 / n2));
+                    break;
+                } else {
+                    System.out.println("no divisible para 0");
+                }
+                break;
+            default:
+                System.out.println("elija el menu la opcion correcta");
+        }*/
+//ESTRUCTURA REPETITIVA: for 
+        /*  int t = 4;
+        for (int i = 1; i <= 15; i++) {
+            System.out.println(t + "x" + i + "=" + (i * t));
+
+        }*/
+//ESTRUCTURA DE REPETICION:WHILI
+// DECLARAR UN VECTOR
+        int[] enteros;
+
+// INICIALIZAR UN VECTOR 
+        enteros = new int[n];
+        enteros[0] = 5;
+        enteros[1] = 7;
+        enteros[2]
+                = 9;
+
+        for (int i = 0; i <= 2; i++) {
+            System.out.println(enteros[i]);
+
+        }
+    
+
+]
